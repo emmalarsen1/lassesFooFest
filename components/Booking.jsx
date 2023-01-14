6c9b16777b3804c27228bcf54cb52a7cd732076a
Lasse Godtkjaer <lass417r@stud.kea.dk>
2023-01-14 20:47:55 +0100

    finalfinal

--- components/Booking.jsx
+++ components/Booking.jsx
@@ -21,24 +21,27 @@ function Booking({ setState, state, isBlured, setBlured }) {
         </svg>
       </div>
       <div className={styles.menu + " " + styles[state]}>
-        <div
-          className={styles.close_btn}
-          onClick={() => {
-            setState("close");
-            setBlured(false);
-          }}
-        >
-          <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" className="bi bi-x" viewBox="0 0 16 16">
-            <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z" />
-          </svg>
-        </div>
-        <div
-          className={styles.blur_btn}
-          onClick={() => {
-            setBlured(!isBlured);
-          }}
-        >
-          {!isBlured ? "BLUR IT" : "UNBLUR IT"}
+        <div className={styles.btnWrapper}>
+          <div
+            className={styles.close_btn}
+            onClick={() => {
+              setState("close");
+              setBlured(false);
+            }}
+          >
+            <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" className="bi bi-x" viewBox="0 0 16 16">
+              <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z" />
+            </svg>
+          </div>
+
+          <div
+            className={styles.blur_btn}
+            onClick={() => {
+              setBlured(!isBlured);
+            }}
+          >
+            {!isBlured ? "BLUR" : "UNBLUR"}
+          </div>
         </div>
         <BookingForm />
       </div>
